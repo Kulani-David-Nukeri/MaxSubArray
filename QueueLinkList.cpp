2672fa0c3bc9b499b46785c33b71f63f8e078842
BennethV <vulombev@gmail.com>
2016-08-25 00:41:20 +0200

    Update QueueLinkList.cpp
    So decided to implement the queue as a doubly linked list. I did not change the names of the function as David asked as of yet... Well I'll do so or he could just do it himself after debugging the code. Here's a checklist of what you requested:
    1. private member queueSize 
    2. int getQueueSize()
    3. queueSize initialisez to zero
    4. quequeSize is incremented when queue() function is called and decremented when dequeue() is called
    
    PLEASE NOTE THAT I COULD HAVE MISTAKENLY CHANGED VARIABLE NAMES THAT I WASN'T SUPPOSED TO CHANGE.
    ( I will leave a "test.cpp" code for you guys to run (compile it at the same directory as output.txt). I have overrided the output operator so that you can see that it works, in case it gets broken :[)

--- QueueLinkList.cpp
+++ QueueLinkList.cpp
@@ -1,23 +1,17 @@
-//
-//  QueueLinkList.cpp
-//  MaxSubArray
-//
-//  Created by Tyson on 2016/08/24.
-//  Copyright © 2016 Tyson Cross. All rights reserved.
-//
-
-#include <stdlib.h>
-#include <stdio.h>
-#include "QueueLinkList.h"
-
 QueueLinkList::QueueLinkList(){
-    head = NULL;
-    tail = NULL;
-    element_size = 0;
+    head = new Node();
+    tail = new Node();
+    head ->next = tail;
+    tail ->prev = head;
+    queueSize = 0;
 }
-
+QueueLinkList::~QueueLinkList(){
+    while(!isEmpty()) dequeueFront();
+    delete head;
+    delete tail;
+}
 bool QueueLinkList::isEmpty() const{
-    return (head == NULL);
+    return (head ->next == tail);
 }
 
 const Node& QueueLinkList::front() const{
@@ -28,33 +22,54 @@ const Node& QueueLinkList::rear() const{
     return *tail;
 }
 
-int QueueLinkList::size() const{
-    return element_size;
+int QueueLinkList::getQueueSize() const{
+    return queueSize;
 }
 
-void QueueLinkList::enqueue(const int& a, const int& b, const int& c){
+void QueueLinkList::add(Node* inputNode,const int& a, const int& b, const int& c){
     Node* newNode = new Node();
     newNode->brute = a;
     newNode->divCon = b;
     newNode->kadane = c;
-    newNode->next = head;
-    head = newNode;
+
+    newNode->next = inputNode;
+    newNode ->prev = inputNode ->prev;
+    inputNode ->prev -> next = inputNode ->prev = newNode;
+}
+void QueueLinkList::enqueue(const int& a, const int& b, const int& c){
+
+    add(head->next,a, b,c);
+     queueSize ++;
+
 }
 
 
+void QueueLinkList::remove(Node* input){
+    Node* prevNode = input -> prev;
+    Node* newNode = input ->next;
+    prevNode ->next = newNode;
+    newNode ->prev = prevNode;
+}
 void QueueLinkList::dequeue(){
-}
 
+    remove(tail -> prev);
+    queueSize --;
+}
+void QueueLinkList::dequeueFront(){
+    remove(head -> next);
+    queueSize --;
+}
+/*
 std::ostream& operator<<(std::ostream& out, const QueueLinkList& obj){
-    Node* temp = obj.head;
-    while(temp !=NULL){
+    Node* temp = obj.tail;
+    int n =1;
+    temp = temp->prev;
+    while( n < obj.getQueueSize()){
+        temp = temp->prev;
         out << temp->brute << ' ' << temp->divCon << ' ' << temp->kadane << std::endl;
-        temp = temp->next;
+
+        n++;
     }
     return out;
 }
-
-QueueLinkList::~QueueLinkList(){
-    while(!isEmpty()) dequeue();
-}
-
+*/
